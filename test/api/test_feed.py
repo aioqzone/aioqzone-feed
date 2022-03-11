9fd060a5d1bba3c048d3f2158a4d3b8446b798de
JamzumSum <zzzzss990315@gmail.com>
2022-03-12 07:22:19 +0800

    test: seperate some fixtures with test logic test: skip when login error ci: re-add cache step

--- test/api/test_feed.py
+++ test/api/test_feed.py
@@ -1,48 +1,14 @@
-import asyncio
-
 import pytest
 import pytest_asyncio
 from aiohttp import ClientSession
 from aioqzone.api.loginman import MixedLoginMan
-from aioqzone.interface.hook import LoginEvent, QREvent
+from aioqzone.exception import LoginError
 
 from aioqzone_feed.api.feed import FeedApi
 from aioqzone_feed.interface.hook import FeedEvent
 from aioqzone_feed.type import FeedContent
 
 pytestmark = pytest.mark.asyncio
-
-
-@pytest.fixture(scope="module")
-def event_loop():
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest_asyncio.fixture(scope="module")
-async def sess():
-    async with ClientSession() as sess:
-        yield sess
-
-
-@pytest_asyncio.fixture(scope="module")
-async def man(sess: ClientSession):
-    from os import environ as env
-
-    man = MixedLoginMan(
-        sess,
-        int(env["TEST_UIN"]),
-        env.get("TEST_QRSTRATEGY", "forbid"),  # forbid QR by default.
-        pwd=env.get("TEST_PASSWORD", None),
-    )
-
-    class inner_qrevent(QREvent, LoginEvent):
-        async def QrFetched(self, png: bytes):
-            showqr(png)
-
-    man.register_hook(inner_qrevent())
-    yield man
 
 
 @pytest_asyncio.fixture(scope="module")
@@ -71,22 +37,13 @@ class FeedEvent4Test(FeedEvent):
         self.drop.append(bid)
 
 
-def showqr(png: bytes):
-    import cv2 as cv
-    import numpy as np
-
-    def frombytes(b: bytes, dtype="uint8", flags=cv.IMREAD_COLOR) -> np.ndarray:
-        return cv.imdecode(np.frombuffer(b, dtype=dtype), flags=flags)
-
-    cv.destroyAllWindows()
-    cv.imshow("Scan and login", frombytes(png))
-    cv.waitKey()
-
-
 async def test_by_count(api: FeedApi):
     hook = FeedEvent4Test()
     api.register_hook(hook)
-    n = await api.get_feeds_by_count(10)
+    try:
+        n = await api.get_feeds_by_count(10)
+    except LoginError as e:
+        pytest.skip(str(e))
     done, pending = await api.wait()
     assert not pending
     assert len(hook.batch) == n - len(hook.drop)
@@ -99,7 +56,10 @@ async def test_by_count(api: FeedApi):
 async def test_by_second(api: FeedApi):
     hook = FeedEvent4Test()
     api.register_hook(hook)
-    n = await api.get_feeds_by_second(3 * 86400)
+    try:
+        n = await api.get_feeds_by_second(3 * 86400)
+    except LoginError as e:
+        pytest.skip(str(e))
     done, pending = await api.wait()
     assert not pending
     assert len(set(hook.batch)) == len(hook.batch) - len(hook.drop)
