1181ee96ae45ec640267eadbe82218f2f1f705d4
JamzumSum <zzzzss990315@gmail.com>
2022-10-24 10:25:40 +0800

    feat(api): expose heartbeat_refresh method

--- src/aioqzone_feed/api/feed.py
+++ src/aioqzone_feed/api/feed.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import time
+from functools import partial
 from typing import Any, Awaitable, Callable, Optional, Set, Tuple, TypeVar
 
 import aioqzone.api as qapi
@@ -311,62 +312,74 @@ class FeedApi(Emittable[FeedEvent]):
 
         super().clear("dispatch")
 
+    async def heartbeat_refresh(self, *, retry: int = 5, retry_intv: float = 5):
+        """A wrapper function that calls :external:meth:`aioqzone.api.DummyQapi.get_feeds_count`
+        and handles all kinds of excpetions raised during heartbeat.
+
+        .. note::
+            This method calls heartbeat **ONLY ONCE** so it should be called circularly by using
+            `.add_heartbeat` or other timer/scheduler.
+
+        :param retry: retry times on QzoneError
+        :param retry_intv: retry interval on QzoneError
+        :return: whether the timer should stop, means heartbeat will always fail until something is changed.
+        """
+        exc, r = None, False
+        for i in range(retry):
+            try:
+                cnt = (await self.api.get_feeds_count()).friendFeeds_new_cnt
+                log.debug("heartbeat: friendFeeds_new_cnt=%d", cnt)
+                if cnt:
+                    self.add_hook_ref("hook", self.hook.HeartbeatRefresh(cnt))
+                return False  # don't stop
+            except (
+                QzoneError,
+                HTTPStatusError,
+            ) as e:
+                # retry at once
+                exc, excname = e, e.__class__.__name__
+                log.warning("%s captured in heartbeat, retry at once (%d)", excname, i)
+                log.debug(excname, exc_info=e)
+            except (
+                HTTPError,
+                SkipLoginInterrupt,
+                UserBreak,
+                asyncio.CancelledError,
+            ) as e:
+                # retry in next trigger
+                exc, excname = e, e.__class__.__name__
+                log.warning("%s captured in heartbeat, retry in next trigger", excname)
+                log.debug(excname, exc_info=e)
+                break
+            except LoginError as e:
+                if e.strategy != QrStrategy.force:
+                    # login error means all methods failed.
+                    # we should stop HB if up login will fail.
+                    r = True
+                break
+            except BaseException as e:
+                exc, r = e, True
+                log.error("Uncaught error in heartbeat.", exc_info=e)
+                break
+            await asyncio.sleep(retry_intv)
+        else:
+            log.error("Max retry exceeds (%d)", retry)
+
+        if r:
+            log.warning(f"Heartbeat stopped.")
+
+        self.add_hook_ref("hook", self.hook.HeartbeatFailed(exc))
+        return r  # stop at once
+
     def add_heartbeat(self, *, retry: int = 5, retry_intv: float = 5, hb_intv: float = 300):
-        """create a heartbeat task and keep a ref of it.
+        """A helper function that creates a heartbeat task and keep a ref of it.
+        A heartbeat task is a timer that circularly calls `.heartbeat_refresh`.
 
         :param retry: max retry times when some exceptions occurs, defaults to 5.
         :param hb_intv: retry interval, defaults to 5.
         :param hb_intv: heartbeat interval, defaults to 300.
         :return: the heartbeat task
         """
-
-        async def heartbeat_refresh():
-            exc, r = None, False
-            for i in range(retry):
-                try:
-                    cnt = (await self.api.get_feeds_count()).friendFeeds_new_cnt
-                    log.debug("heartbeat: friendFeeds_new_cnt=%d", cnt)
-                    if cnt:
-                        self.add_hook_ref("hook", self.hook.HeartbeatRefresh(cnt))
-                    return False  # don't stop
-                except (
-                    QzoneError,
-                    HTTPStatusError,
-                ) as e:
-                    # retry at once
-                    exc, excname = e, e.__class__.__name__
-                    log.warning("%s captured in heartbeat, retry at once (%d)", excname, i)
-                    log.debug(excname, exc_info=e)
-                except (
-                    HTTPError,
-                    SkipLoginInterrupt,
-                    UserBreak,
-                    asyncio.CancelledError,
-                ) as e:
-                    # retry in next trigger
-                    exc, excname = e, e.__class__.__name__
-                    log.warning("%s captured in heartbeat, retry in next trigger", excname)
-                    log.debug(excname, exc_info=e)
-                    break
-                except LoginError as e:
-                    if e.strategy != QrStrategy.force:
-                        # login error means all methods failed.
-                        # we should stop HB if up login will fail.
-                        r = True
-                    break
-                except BaseException as e:
-                    exc, r = e, True
-                    log.error("Uncaught error in heartbeat.", exc_info=e)
-                    break
-                await asyncio.sleep(retry_intv)
-            else:
-                log.error("Max retry exceeds (%d)", retry)
-
-            if r:
-                log.warning(f"Heartbeat stopped.")
-
-            self.add_hook_ref("hook", self.hook.HeartbeatFailed(exc))
-            return r  # stop at once
-
+        heartbeat_refresh = partial(self.heartbeat_refresh, retry=retry, retry_intv=retry_intv)
         self.hb_timer = AsyncTimer(hb_intv, heartbeat_refresh, delay=hb_intv)
         return self.hb_timer()
