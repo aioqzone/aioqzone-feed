d0ae33573e0ebd3caee8e13c73d7e6a9e720c734
JamzumSum <zzzzss990315@gmail.com>
2023-11-30 14:57:36 +0800

    feat(hb): handle ClientResponseError

--- test/api/test_heartbeat.py
+++ test/api/test_heartbeat.py
@@ -1,16 +1,26 @@
-from typing import Type
+from typing import Type, cast
 from unittest.mock import patch
 
 import pytest
 import pytest_asyncio
+from aiohttp import ClientResponseError, RequestInfo
 from aioqzone.api import Loginable
+from multidict import CIMultiDictProxy
 from qqqr.exception import UserBreak
 from qqqr.utils.net import ClientAdapter
 from tenacity import Future, RetryError
+from yarl import URL
 
 from aioqzone_feed.api import FeedApi
 
 pytestmark = pytest.mark.asyncio
+
+_fake_request = RequestInfo(
+    URL("https://mobile.qzone.qq.com/feeds/mfeeds_get_count"),
+    "GET",
+    cast(CIMultiDictProxy, ...),
+    URL(),
+)
 
 
 @pytest_asyncio.fixture
@@ -25,6 +35,7 @@ async def api(client: ClientAdapter, man: Loginable):
     [
         (SystemExit(),),
         (RetryError(Future.construct(1, UserBreak(), True)),),
+        (ClientResponseError(_fake_request, (), code=500)),
     ],
 )
 async def test_heartbeat_exc(api: FeedApi, exc2r: Type[BaseException]):
